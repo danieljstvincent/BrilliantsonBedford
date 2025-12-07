d5ae7ea7037d21e4e708e73a7c6161fe6d617768
danielstvincent <dannystvincent@gmail.com>
2025-12-06 19:55:45 -0500

    good

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  "./src/**/*.{js,jsx,ts,tsx}", // Keep this if you use src
+  "./app/**/*.{js,jsx,ts,tsx}", // Add this if you have an app folder at root
+  "./pages/**/*.{js,jsx,ts,tsx}", // Add this if you have a pages folder at root
+  "./components/**/*.{js,jsx,ts,tsx}", // Add this if you have a components folder at root
+],
   theme: {
     extend: {},
   },
